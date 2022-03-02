e456e97220848148778c291ae597a0b40888dcd3
emailmeydrive <emailme.ydrive@gmail.com>
2022-03-02 14:34:35 +0200

    fbjcb3r

--- pac.py
+++ pac.py
@@ -1,3 +1,5 @@
+from ast import If
+from turtle import color
 import pygame
  
 WINDOW_W = 1336
@@ -13,7 +15,7 @@ pygame.display.set_caption("PAC - MAN --> ziv")
 bk = pygame.image.load("PACPAC.png")
 bk = pygame.transform.scale(bk,(WINDOW_W,WINDOW_H))
 pac = pygame.image.load("PACLOL.png")
-pac = pygame.transform.scale(pac,(40,40))
+pac = pygame.transform.scale(pac,(30,30))
 clock = pygame.time.Clock()
 
 
@@ -21,10 +23,10 @@ pac_x = WINDOW_W /2
 pac_y = WINDOW_H - 80
 
 pac_x_step = 10
-x_step = 10
+x_step = 7
 play = True
 
-y_step = 10
+y_step = 7
 
 myfont = pygame.font.SysFont('Comic Sans MS', 30)
 textsurface = myfont.render('Score:', True, (255, 255, 255))
@@ -38,7 +40,8 @@ vector4 = False
 while play:
   screen.blit(bk,(0,0))
   screen.blit(pac,(pac_x,pac_y))
-
+  color = screen.get_at((int(pac_x+ 15),int(pac_y - 4)))
+  print(color)
   # pygame.draw.rect(screen,(24,116,205),((20,15),(1300,15)))
   # pygame.draw.rect(screen,(24,116,205),((20,480),(1300,480)))
 
@@ -56,17 +59,31 @@ while play:
   # pygame.draw.rect(screen,(24,116,205),((1000,100),(70,250)))
   keys = pygame.key.get_pressed()
   if keys[pygame.K_LEFT]:
-    pac_x -= x_step
+    if color == (0,0,1,255):
+      pac_x -= x_step
   if keys[pygame.K_RIGHT]:
     pac_x += x_step
+  if keys[pygame.K_UP]:
+    pac_y -= y_step
+  if keys[pygame.K_DOWN]:
+    pac_y += y_step
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       play = False
     elif event.type == pygame.KEYDOWN:
       if event.key == pygame.K_LEFT:
-        pac_x -= x_step
+        if color == (0,0,1,255):
+          pac_x -= x_step
       if event.key == pygame.K_RIGHT:
         pac_x += x_step
+      if event.key == pygame.K_UP:
+        pac_y -= y_step
+      if event.key == pygame.K_DOWN:
+        pac_y += y_step
+  if pac_x < 0:
+    pac_x += WINDOW_W
+  if pac_x > WINDOW_W:
+    pac_x -= WINDOW_W
   pygame.display.flip()
 
 
